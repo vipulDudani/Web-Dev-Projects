9f4440afa8eebb809f767b24c9bf4a4d2e81ef49
vipulDudani <128981674+vipulDudani@users.noreply.github.com>
2023-04-14 14:22:08 +0530

    Add files via upload

---
+++ BMI/script.js
@@ -0,0 +1,14 @@
+document.getElementById('bmiForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Get weight and height inputs
+    let weight = parseFloat(document.getElementById('weight').value);
+    let height = parseFloat(document.getElementById('height').value);
+    
+    // Calculate BMI
+    let bmi = weight / (height * height);
+    
+    // Display result
+    document.getElementById('result').innerText = 'Your BMI: ' + bmi.toFixed(2);
+  });
+  
