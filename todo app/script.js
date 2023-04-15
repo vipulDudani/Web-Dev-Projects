26ffdd003a261c81ae5a4b6bb2d64e1a76429e15
vipulDudani <128981674+vipulDudani@users.noreply.github.com>
2023-04-15 08:46:17 +0530

    Update script.js

--- todo app/script.js
+++ todo app/script.js
@@ -1,1 +1,33 @@
+// script.js
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+const taskList = document.getElementById('taskList');
+
+// Add task event listener
+addBtn.addEventListener('click', addTask);
+
+// Add task function
+function addTask() {
+    const taskText = taskInput.value;
+    if (taskText.trim() !== '') {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>${taskText}</span>
+            <button class="deleteBtn">Delete</button>
+        `;
+        taskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
+
+// Delete task event listener
+taskList.addEventListener('click', deleteTask);
+
+// Delete task function
+function deleteTask(event) {
+    if (event.target.classList.contains('deleteBtn')) {
+        const li = event.target.closest('li');
+        li.remove();
+    }
+}
 
